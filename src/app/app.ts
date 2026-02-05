c64c2a023b9b1b7c47d837637ae0321a967eb205
BobDoland <46633981+BobDoland@users.noreply.github.com>
2026-02-05 14:09:22 -0600

    ran prettier again on whole project I guess

--- src/app/app.ts
+++ src/app/app.ts
@@ -49,7 +49,7 @@ export class AppComponent {
   }
 
   profile(): void {
-    this.router.navigate(['/profile/'+this.authService.username]);
+    this.router.navigate(['/profile/' + this.authService.username]);
   }
 
   showNavBar(): boolean {
